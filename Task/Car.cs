9ca5a84c4ea1f86ae952d54b5ee08215456a0a95
Alizadehh1 <alisal@code.edu.az>
2021-12-14 14:02:45 -0800

    first

---
+++ Task/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Car
+    {
+        public string marka;
+        public string model;
+        public double price;
+        public int year;
+        public string GetFullName()
+        {
+            return $"Marka-{marka}, Model-{model}, Price-{price}, Year-{year}";
+
+        }
+    }
+
+}
---
+++ Task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many cars do you want to upload: ");
+            int len=Convert.ToInt32(Console.ReadLine());
+            Car[] Cars= new Car[len];
+            
+            for(int i=0; i<Cars.Length; i++)
+            {
+                Car cars= new Car();
+
+                Console.Write($"Brand of Car {i+1}: ");
+                cars.marka=Console.ReadLine();
+
+                Console.Write($"Model of Car {i + 1}: ");
+                cars.model = Console.ReadLine();
+
+                Console.Write($"Price of Car {i + 1}: ");
+                cars.price=Convert.ToDouble(Console.ReadLine());
+
+                Console.Write($"Year of Car {i + 1}: ");
+                cars.year = Convert.ToInt32(Console.ReadLine());
+
+                Cars[i] = cars;
+
+            }
+            foreach(var car in Cars)
+            {
+                string name = car.GetFullName();
+                Console.WriteLine("Your Car Properties : " + name);
+            }
+
+
+        }
+    }
+}
